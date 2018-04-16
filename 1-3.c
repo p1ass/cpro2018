e98dd029f623df39a3591e0e19aeb27d72280eff
plusl <tennis48691124@gmail.com>
2018-04-16 20:30:31 +0900

    change pritnf method

--- 1-3.c
+++ 1-3.c
@@ -22,7 +22,7 @@ int main(void){
     x1 = (-b + sqrt(pow(b,2)-4*a*c)) / 2*a; 
     x2 = (-b - sqrt(pow(b,2)-4*a*c)) / 2*a; 
 
-    printf("Answers are\n %.3lf and %.3lf.",x1,x2);
+    printf("Answers are\n %.3f and %.3f.",x1,x2);
 
     return 0;
 }
