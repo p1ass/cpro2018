ecd3cdfbb48fcb10c90f4c02d5954b2bcb92603b
plusl <tennis48691124@gmail.com>
2018-04-18 10:25:55 +0900

    modify printf method

--- 1-3.c
+++ 1-3.c
@@ -11,18 +11,17 @@ int main(void){
 
     double x1,x2;
 
-    printf("solve ax^2+bx+c=0\n\n");
-    printf("please input \"a\"\na = ");
+    printf("a = ");
     scanf("%lf",&a);
-    printf("please input \"b\"\nb = ");
+    printf("b = ");
     scanf("%lf",&b);
-    printf("please input \"c\"\nc = ");
+    printf("c = ");
     scanf("%lf",&c);
 
-    x1 = (-b + sqrt(pow(b,2)-4*a*c)) / 2*a; 
-    x2 = (-b - sqrt(pow(b,2)-4*a*c)) / 2*a; 
+    x1 = (-b + sqrt(pow(b,2)-4*a*c)) / 2*a;
+    x2 = (-b - sqrt(pow(b,2)-4*a*c)) / 2*a;
 
-    printf("Answers are\n %.3f and %.3f.",x1,x2);
+    printf("%f \n%f\n",x1,x2);
 
     return 0;
 }
