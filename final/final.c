3fa046cd9d7a0057618708bf87fae8e9a642ac46
NaokiKishi <naokikishi.kyoto@gmail.com>
2018-05-23 00:47:28 +0900

    change index method

--- final/final.c
+++ final/final.c
@@ -7,13 +7,13 @@ void copy(int m, int n, const float *x, float *y);
 void fc(int m,int n,const float *A,const float *b,const float *x, float *y);
 void ReLU(int n,const float *x, float *y);
 void softmax(int n,const float *x, float *y);
-int inference3(const float *A, const float *b, const float *x);
+int inference3(const float *A, const float *b, const float *x,float * y);
 void softmaxwithloss_bwd(int n, const float *y, unsigned char t, float *dEdx);
 void relu_bwd(int n, const float * x, const float * dEdy, float * dEdx);
 void fc_bwd(int m, int n, const float * x, const float * dEdy, const float * A, float * dEdA, float * dEdb, float * dEdx);
 void backward3(const float *A, const float *b, const float *x, unsigned char t, float *y, float *dEdA, float *dEdb);
 void shuffle(int n,int * x);
-float cross_entropy_error(const float  y, int t);
+float cross_entropy_error(const float * y, int t);
 void add(int n, const float *x, float *o);
 void scale(int n, float x, float *o);
 void init(int n, float x, float *o);
@@ -51,7 +51,7 @@ int main()
     float *A = malloc(sizeof(float)* 784*10);
     float *b = malloc(sizeof(float)*10);
     int * index = malloc(sizeof(int)*train_count);
-    int epoch = 10;
+    int epoch = 20;
     int batch = 100;
     float h = 0.01;
     int i,j,k,l,m;
@@ -59,15 +59,16 @@ int main()
 
     rand_init(784*10,A);
     rand_init(10,b);
-
     //エポックを回す
     for(i=0;i<epoch;i++){
-        printf("Epoch %d\n", i+1);
+        printf("Epoch %d/%d\n", i+1,epoch);
         //インデックスを作成し、並び替え
 
-        for(l=0 ; l<train_count ;l++) {
+        for(l=0 ; l<train_count ; l+=1) {
             index[l] = l;
+            // printf("%d\n",index[i]);
         }
+
         shuffle(train_count, index);
 
         //ミニバッチ
@@ -79,34 +80,40 @@ int main()
 
             //一つ一つの勾配を計算する
             for(k=0;k<batch;k++){
-                backward3(A,b,train_x+ 784*index[k] ,train_y[index[k]],y,dEdA,dEdb);
+                backward3(A,b,train_x+ 784*index[j*batch+k] ,train_y[index[j*batch+k]],y,dEdA,dEdb);
                 add(784*10,dEdA,dEdA_av);
                 add(10,dEdb,dEdb_av);
             }
+            // print(1,10,dEdb_av);
 
+            // print(1,10,dEdb_av);
             //ミニバッチで割って平均を求める
-            scale(784*10,1/batch,dEdA_av);
-            scale(10,1/batch,dEdb_av);
+            scale(784*10,-h/batch,dEdA_av);
+            scale(10,-h/batch,dEdb_av);
+            // print(1,10,dEdb_av);
 
             //係数A,bを更新
-            scale(784*10,-1*h,dEdA_av);
-            add(784*10,dEdA_av,dEdA);
+            add(784*10,dEdA_av,A);
+            add(10,dEdb_av,b);
 
-            scale(10,-1*h,dEdb_av);
-            add(10,dEdb_av,dEdb);
 
         }
-        printf("Complete Epoch %d\n",i+1);
+
         //テストデータでの推論
         int sum = 0;
-        for(m=0 ; i<test_count ; m++) {
-            if(inference3(A, b, test_x + m*width*height) == test_y[m]) {
+        float loss_sum = 0;
+        // printf("test_count : %d\n",test_count);
+        for(m=0 ; m<test_count ; m++) {
+            if(inference3(A, b, test_x + m*width*height,y) == test_y[m]) {
                 sum++;
             }
+            loss_sum += cross_entropy_error(y,test_y[m]);
+
         }
         acc = sum * 100.0 / test_count;
 
-        printf("Accuracy : %f",acc);
+        printf("Accuracy : %f ％ \n",acc);
+        printf("Loss Average : %f\n",loss_sum/test_count);
     }
 
     return 0;
@@ -188,11 +195,12 @@ int main()
     }
 
     //3層による推論
-    int inference3(const float *A, const float *b, const float *x){
+    int inference3(const float *A, const float *b, const float *x,float * y){
         int m = 784;
         int n = 10;
-        float *y = malloc(sizeof(float) * 10);
+        // float *y = malloc(sizeof(float) * 10);
         // float y[10];
+
         fc(m, n, A, b, x, y);
         ReLU(n, y, y);
         softmax(n, y, y);
@@ -207,7 +215,7 @@ int main()
                 index = i;
             }
         }
-        free(y);
+        // free(y);
         return index;
     }
 
@@ -255,7 +263,7 @@ int main()
         }
 
         for (i = 0; i < n; i++){
-
+            dEdx[i] = 0;
             for (j = 0; j < m; j++){
                 dEdx[i] += A[j * n + i] + dEdy[j];
             }
@@ -286,7 +294,7 @@ int main()
         int i, j;
 
         for (i = 0; i < n; i++){
-            j = (int)(rand() * n / (1.0 + RAND_MAX));
+            j = (int)(rand()*(  n+1.0)/(1.0+RAND_MAX));
             int tmp_xi = x[i];
             x[i] = x[j];
             x[j] = tmp_xi;
@@ -294,8 +302,8 @@ int main()
     }
 
     //損失関数を求める
-    float cross_entropy_error(const float y, int t){
-        return t * log(y + 1e-7);
+    float cross_entropy_error(const float * y, int t){
+        return -1 *log(y[t] + 1e-7);
     }
 
     void add(int n, const float *x, float *o){
@@ -326,6 +334,6 @@ int main()
         int i;
 
         for (i = 0; i < n; i++){
-            o[i] = (rand() * 2 / (1.0 + RAND_MAX)) - 1.0;
+            o[i] = ((float)rand() / ((float)RAND_MAX + 1)) * 2 - 1 ;
         }
     }
