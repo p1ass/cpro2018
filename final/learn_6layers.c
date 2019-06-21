7906a7514c26777327be26c3c1de354842b52760
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-06-22 00:40:05 +0900

    リファクタリング

--- final/learn_6layers.c
+++ final/learn_6layers.c
@@ -73,7 +73,7 @@ void learn_6layers(int train_count,int test_count,float * train_x,unsigned char 
 
     //変数初期化
     int * index = malloc(sizeof(int)*train_count);
-    int epoch = 10;
+    int epoch = 20;
     int batch = 100;
     float h = 0.01;
     int i,j,k,l,m;
@@ -82,12 +82,13 @@ void learn_6layers(int train_count,int test_count,float * train_x,unsigned char 
     // rand_init(784*50,A1);
     // rand_init(50*100,A2);
     // rand_init(100*10,A3);
-    init(50,0,b1);
-    init(100,0,b2);
-    init(10,0,b3);
     rand_init_by_normal_dist(784*50,A1,0,sqrt(2.0/784));
     rand_init_by_normal_dist(50*100,A2,0,sqrt(2.0/50));
     rand_init_by_normal_dist(100*10,A3,0,sqrt(2.0/100));
+    
+    init(50,0,b1);
+    init(100,0,b2);
+    init(10,0,b3);
     // rand_init_by_normal_dist(50,b1,0,sqrt(2.0/50));
     // rand_init_by_normal_dist(100,b2,0,sqrt(2.0/100));
     // rand_init_by_normal_dist(10,b3,0,sqrt(2.0/10));
